f364c833fa1b8c171d2e6a09b760edff150698bc
Anders <idris.maps@gmx.ch>
2021-07-11 10:06:07 +0200

    Svelte and vue components

---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,6 @@
+<script>
+  let count = 0
+  const inc = () => count = count + 1
+</script>
+
+<button on:click={inc}>Svelte counter {count}</button>
---
+++ src/components/VueCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="counter">
+    <button @click="inc()">Vue counter {{ count }}</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const count = ref(0)
+    const inc = () => count.value = count.value + 1
+    return {
+      count,
+      inc,
+    }
+  }
+}
+</script>
